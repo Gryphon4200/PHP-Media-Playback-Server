0bdecdf859d09d3933e17933028a209bc33eccaf
Chris Hamby <gryphon4200@gmail.com>
2023-03-23 10:52:05 -0500

    Added preset option.

--- update.php
+++ update.php
@@ -1,6 +1,25 @@
 <?php
-  $filename = $_POST["filename"];
-  $timestamp = $_POST["timestamp"];
+  if (isset($_GET["preset"])) {
+    $timestamp = time();
+
+    switch ($_GET["preset"]) {
+      case 1:
+        $filename = "MorningLine Skyline.jpg";
+        break;
+      case 2:
+        $filename = "OpenLine.png";
+        break;
+      case 3:
+        $filename = "InsidePolotics.png";
+        break;                
+      default:
+        $filename = "NC5-Wallpaper.png";
+        break;
+    }
+  } else {
+    $filename = $_POST["filename"];
+    $timestamp = $_POST["timestamp"];
+  }
 
   $text = $filename . "|" . $timestamp;
   $file = fopen("image.txt", "w") or die("Unable to open file!");
